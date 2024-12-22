dc8f34a1471c1ae6f784112bf829f5487c4e1d59
Martín Fernández <martinffg@gmail.com>
2024-12-22 14:01:05 -0300

    Easy Mode completo + Implementación de ingesta de datos por JSON/API por lo que califica como candidato al premioo

---
+++ README - Ultimas directivas TFI.txt
@@ -0,0 +1,33 @@
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━•(=^●ω●^=)•━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+Trabajo Práctico Final Integrador (Borrador sujeto a modificaciones)
+
+> Fechas Importantes
+9/12: Apertura en campus para entrega de proyectos finales.
+23/12: Fecha límite de entrega de Proyectos.
+30/12: Fecha límite para corrección de Proyectos.
+13/01: Descarga de Diplomas: Para que suceda esto, el estudiante debe tener el aprobado en el aula virtual (Tarea del Instructor) y completar la encuesta de satisfacción, de este  modo se va a habilitar el diploma para su descarga.
+
+> Formato de Entrega
+Compartir link de Github Pages o Netlify en el apartado de entrega en el Campus Virtual (Requerido)
+
+
+> Requisitos para la entrega
+
+**Modo Easy**
+1. En una sección cargar los items en forma de cards.
+2. Tener un botón que muestre la descripción extensa del item.
+
+**Modo Hard**
+1. Implementar el Carrito con LocalStorage
+2. Consumir una API (de su agrado) y mostrar los datos en una sección.
+3. Darle un toque personal. Sugerencias: agregar el botón menos info, cantidad de productos , modal, agregar a la card imagen, usar un fichero json externo, etc.
+
+> Premio
+**50.000 $** 
+
+Requisitos para el premio 
+Entregar un proyecto con alguna/s características del Hard Mode.
+Entregar el 23/12 o antes en el Aula Virtual.
+
+
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━•(=^●ω●^=)•━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
--- js/catalogo_funciones.js
+++ js/catalogo_funciones.js
@@ -6,13 +6,13 @@
 //   }
 // });
 
-function insertarCard(producto, i, productos) {
-    const card = crearCard(producto);
+function insertarCard(producto, id, productos) {
+    const card = crearCard(producto, id, productos);
     controlPrecio(card);
     renderizarCard(card);
   }
   
-  function crearCard(producto) {
+  function crearCard(producto, id, productos) {
     const divCard =document.createElement('div');
     const img = document.createElement('img');
   
@@ -20,20 +20,84 @@ function insertarCard(producto, i, productos) {
     divCard.classList.add('card');
   
     // desestructuración de array
-    // const {nombre, precio, anio, urlImg}  =   producto;
+    // const {nombre, precio, anio, urlImg, descripcion}  =   producto;
     const nombre = producto.nombre;
     const precio = producto.precio;
     const anio = producto.anio;
     const urlDownload = producto.urlDownload;
     const urlImg = producto.urlImg;
+    const descripcion = producto.descripcion;
        
     divCard.innerHTML = `
-       <a href="${producto.urlDownload}"><img src="${producto.urlImg}" alt="Imagen del producto ${nombre}"></a> 
+       <a href="${urlDownload}"><img src="${urlImg}" alt="Imagen del producto ${nombre}"></a> 
        <h2>${nombre}</h2>
        <p>Precio USD: ${precio}</p>
-       <p>Año: ${anio}</p>`;
-  
+       <p>Año: ${anio}</p>
+       <button id="btnMasInfo${id}" class="buttonCatalogo" onclick="masInfoProducto(${id})">+Info</button>
+       <div id="botonera${id}">
+          <button class="buttonCatalogo" value="Comprar" onclick="comprarProducto(${id})">Comprar</button>
+       </div>
+       <div id="divOutput">
+          <output id="description${id}" class="description" style="display:none">
+            ${descripcion}
+            <br>
+              <button id="btnCompra${id}" class="buttonCatalogo" type="submit" value="Comprar" onclick="comprarProducto(${id})">Comprar</button>
+          </output>
+        </div>
+       `;
     return divCard;
+  }
+
+  function masInfoProducto(id){
+    // alert("¿Querés más información sobre " + productos[id].nombre + "?\nDescripción: " + productos[id].descripcion);
+    event.preventDefault();
+    let idx = id.toString();
+    //seleccionamos los elementos, le damos un id unico
+    //y los guardamos en una variable
+    let param = 'description' + idx;
+    let btnMasInfo = 'botonera' + idx;
+    let btnComprar = 'btnMasInfo' + idx;
+    let btnComprarDesc = 'comprar' + idx;
+
+    // console.log(param)
+    // console.log(btnMasInfo)
+    // console.log(btnComprar)
+    // console.log(btnComprarDesc)
+
+    //ocultamos o mostramos los elementos
+    document.getElementById(param).style.display = "block";
+    document.getElementById(btnComprar).style.display = "none";
+    document.getElementById(btnMasInfo).style.display = "none";
+   }
+
+  function comprarProducto(id){
+    alert("¿Querés comprar " + productos[id].nombre + "?\nTodavía falta un poco para que terminemos de implementar el Carrito de Compras, te pedimos por favor que tengas paciencia y recuerda que:\n'TARDA EN LLEGAR Y AL FINAL HAY RECOMPENSA'");
+    // //Extraigo los datos a guardar
+    // let idx = id.toString();
+    // let param = 'my_form' + idx;
+    // let form = document.getElementById(param);
+    // console.info('Tipo ' + typeof form);
+    // const nombre = form.elements['nombre'].value; // accessing element by name
+    // const precio = form.elements['precio'].value; // accessing element by name
+    // //const firstElement = form.elements[0]; // accessing first element by index no.
+    // console.log("Cantidad de elementos: "+ form.length);
+    // console.log("Nombre " + nombre);
+    // console.log("precio2 " + precio);
+
+    // //Armo el objeto a guardar
+    // let pedido = {
+    //   "id" : Date.now(),
+    //   "productoId" : idx,
+    //   "name" : nombre,
+    //   "price" : precio,
+    //   "amount" : 1
+    // }
+    // //LocalStorage
+    // if (typeof(Storage) !== "undefined") {
+    //   localStorage.setItem(pedido.id, JSON.stringify(pedido));
+    // } 
+    
+    // return false;
   }
   
   function controlPrecio(card) {
